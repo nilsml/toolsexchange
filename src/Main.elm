87af88f988d6b75469273177a501f2289b888225
Nils Magne Lunde <nilsml@nilsland.com>
2016-02-03 20:25:45 +0100

    Added some super simple code to main.elm

---
+++ src/Main.elm
@@ -0,0 +1,19 @@
+import Html exposing (div, button, text)
+import Html.Events exposing (onClick)
+import StartApp.Simple as StartApp
+
+
+main =
+  StartApp.start { model = model, view = view, update = update }
+
+
+model = 0
+
+
+view address model =
+  div []
+    [ text "such wow"]
+
+
+update action model =
+  model
