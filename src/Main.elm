feaa22f4934970fcf306013b9d998551f12fba9a
Nils Magne Lunde <nilsml@nilsland.com>
2016-02-03 21:31:48 +0100

    Added a heading with a button

--- src/Main.elm
+++ src/Main.elm
@@ -19,7 +19,19 @@ view address model =
     , stylesheet "../css/tuktuk.grid.css"
     , stylesheet "../css/tuktuk.icons.css"
     , stylesheet "../css/tuktuk.theme.default.css"
-    , h1_ "such wow"
+    , header [ A.class "bck margin-top margin-bottom" ]
+      [ div [ A.class "column_6" ]
+        [ h2 [ A.class "text italic thin" ] [ text "Much amaze!" ]
+        ]
+      ]
+      , nav [ A.class "column_6 text right" ]
+        [ a [ A.href "#"
+            , A.class "button"
+            ]
+              [ span [ A.class "icon user" ] []
+              , text "Login"
+              ]
+        ]
     ]
 
 
@@ -34,3 +46,20 @@ stylesheet href =
     [ A.rel "stylesheet"
     , A.href href
     ] []
+
+
+{-|
+
+ <header class="bck margin-top margin-bottom">
+        <div class="row">
+            <div class="column_6">
+                <img src="/img/product/tuktuk.png" class="logo on-left"/>
+                <h2 class="text italic thin">Landing Template</h2>
+            </div>
+            <nav class="column_6 text right">
+                <a href="#" class="button"><span class="icon user"></span>Login</a>
+                <a href="#" class="button bck theme"><span class="icon pencil"></span>Signup</a>
+            </nav>
+        </div>
+    </header>
+|-}
